955816965cad0f11b6ad70e4f3cd0ddc093d2019
David Oster <paspa@hotmail.com>
2021-03-02 22:49:42 +0200

    combined

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var Timer = require('./timer')
 var TShirt = require('./tshirt')
+var Color = require('./color')
 var BubbleSort = require('./bubblesort')
 var QuickSort = require('./quicksort')
 var BucketSort = require('./bucketsort')
@@ -132,4 +133,74 @@ function doBucketSort() {
     console.log("Sorted Array BUS Fabric DESC")
     sortedByBusFabricDESC.forEach(item => console.log(item.toString()))
 }
-doBucketSort()
+// doBucketSort()
+
+function doBubbleCombined() {
+    // Step 1. Sort (Bubble Sort) by Color ASC
+    // Step 2. Find (if any) same colored TShirts, e.g. 2 REDs
+    // Step 3. Get these same colored TShirts and short them by Size 
+    // Step 4. Find (if any) same sized TShirts, e.g. 3 XS
+    // Step 5. Get these same sized TShirts and sort them by Fabric
+
+    // Step 1
+    var myBS = new BubbleSort();
+    var BSColorASCTimer = new Timer()
+    console.log(tShirts.length)
+    var sortedBSTShirts_Color_ASC = myBS.sort({ data: tShirts, attribute: 'Color', type: 'ASC' }) 
+    BSColorASCTimer.stop()
+    console.log("Sorted Array BS Color ASC")
+    console.log("Elapsed Time: " + BSColorASCTimer.elapsedTime())
+    printArrayOfTShirts(sortedBSTShirts_Color_ASC)
+
+    // Step 2 -- Place the same colors TOGETHER
+    var n = Object.keys(new Color().colors).length
+    // console.log(n)
+    //Create a bucket array
+    let bucket = new Array(n); // ??? is this correct 
+
+    //Add bucket group
+    for (let i = 0; i <= n; i++) {
+        bucket[i] = [];
+    }
+    
+    // console.log(sortedBSTShirts_Color_ASC.length)
+    for (let i = 0; i < sortedBSTShirts_Color_ASC.length; i++) {
+        // console.log(i,sortedBSTShirts_Color_ASC[i]._color)
+        // bucket[1] <--- [].push()
+        bucket[sortedBSTShirts_Color_ASC[i]._color].push(sortedBSTShirts_Color_ASC[i]);
+    }
+
+    // Step 3
+    // Actual sort on every bucket per Size
+    for (let i = 0; i <= n; i++) {
+        //bucket[i].sort();
+        var bucketi = myBS.sort({ data: bucket[i], attribute: 'Size', type: 'ASC' })
+        bucket[i] = bucketi
+    }
+
+    // Step 4
+    // get the bucket and find per Size similar sizes
+    //Create a bucket array
+    let bucketSize = new Array(n); // ??? is this correct 
+
+    //Add bucket group
+    for (let i = 0; i <= n; i++) {
+        bucketSize[i] = [];
+    }
+
+    for (let i = 0; i <= n; i++) {
+        //bucket[i].sort();
+        var bucketi = myBS.sort({ data: bucket[i], attribute: 'Size', type: 'ASC' })
+        bucketSize[i] = bucketi
+    }
+
+    bucketSize.forEach(element => {
+        bucketSize[element._color].push(bucketSize[i]); // ?????????
+    });
+    
+
+}
+    
+
+
+doBubbleCombined()
--- bucketsort.js
+++ bucketsort.js
@@ -3,7 +3,7 @@ var Fabric = require('./fabric')
 class BucketSort {
     bucketSort(arr, n = 7, attribute, type) {
         //Create a bucket array
-        let bucket = new Array(arr.length);
+        let bucket = new Array(arr.length); // ??? is this correct 
 
         //Add bucket group
         for (let i = 0; i <= n; i++) {
@@ -13,9 +13,11 @@ class BucketSort {
         //Add the elements in a same range in bucket
         for (let i = 0; i < arr.length; i++) {
             //let bucketIndex = n //Math.floor(arr[i]) * n;
-            if (attribute == 'Size') bucket[arr[i]._size].push(arr[i]);
+            if (attribute == 'Size') bucket[arr[i]._size].push(arr[i]); // bucket[1] -> XS, RED, WOOL
             if (attribute == 'Color') bucket[arr[i]._color].push(arr[i]);
-            if (attribute == 'Fabric') bucket[Object.values(new Fabric().fabrics).indexOf(arr[i]._fabric)].push(arr[i]);
+            // new Fabric().fabrics -> {WOOL: 3, COTTON: 5, POLYESTER: 6.5, RAYON: 9.2, LINEN: 13.6, CASHMERE: 17.4, SILK: 25.2}
+            console.log(Object.values(new Fabric().fabrics).indexOf(arr[i]._fabric)+1)
+            if (attribute == 'Fabric') bucket[Object.values(new Fabric().fabrics).indexOf(arr[i]._fabric)+1].push(arr[i]);
         }
 
         // //Sort each bucket separately
